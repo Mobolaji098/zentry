b28a4f1f3773b659376bd9dc72dbfb8d5877452c
Barterway <barterwayapp.gmail.com>
2024-12-23 04:24:48 +0100

    autoplay enabled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -106,7 +106,7 @@ setIsLoading(false);
                     />
                     <video
                         src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-                        // autoPlay
+                        autoPlay
                         loop
                         muted
                         className='absolute left-0 top-0 object-cover object-center size-full'
